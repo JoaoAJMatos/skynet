592d86116006608e0ca8f8f83646538a7a0ee0ec
JoaoAJMatos <joaoafonsoj2004@gmail.com>
2023-02-02 14:28:54 +0000

    crypto: Removes unnecessary member variables in ECDSA wrapper class
    The 'randomize' buffer and the 'privkey' buffer weren't needed as Class members and could cause problems. So I added them as local variables to the functions that needed to access them

--- src/crypto/ecdsa/ecdsa.cpp
+++ src/crypto/ecdsa/ecdsa.cpp
@@ -63,13 +63,14 @@ static ErrorCode fill_random(unsigned char* data, size_t size) {
  *        to protect against side-channel leakage.
  */
 crypto::CEcdsa::CEcdsa() {
+    byte randomize[SHA256_DIGEST_LENGTH];
     this->ctx = secp256k1_context_create(SECP256K1_CONTEXT_NONE);
 
-    if (fill_random(this->randomize, sizeof(this->randomize)) != OK) {
+    if (fill_random(randomize, sizeof(randomize)) != OK) {
         throw std::runtime_error("Could not generate randomness for ECDSA");
     }
 
-    if (secp256k1_context_randomize(this->ctx, this->randomize) != 1) {
+    if (secp256k1_context_randomize(this->ctx, randomize) != 1) {
         throw std::runtime_error("Could not randomize ECDSA context");
     }
 }
@@ -80,10 +81,6 @@ crypto::CEcdsa::CEcdsa() {
  */
 crypto::CEcdsa::~CEcdsa() {
     secp256k1_context_destroy(this->ctx);
-
-    // Best practice to clear this from memory
-    memset(this->randomize, 0, sizeof(this->randomize));
-    memset(this->privkey, 0, sizeof(this->privkey));
 }
 
 /**
@@ -94,22 +91,24 @@ crypto::CEcdsa::~CEcdsa() {
  * @param public_key
  * @return ErrorCode - OK if successful, ERROR otherwise
  */
-ErrorCode crypto::CEcdsa::GenerateKeyPair(ecdsa_keypair *keypair) {
+ErrorCode crypto::CEcdsa::GenerateKeyPair(ecdsa_keypair_t *keypair) {
     secp256k1_pubkey pubkey;
     size_t pubkey_len;
+    byte privkey[SHA256_DIGEST_LENGTH];
 
     // If the secret key is 0 or bigger than sec256k1's order, we need to regenerate it.
     // The probability of this happening is negligible, but we still account for it.
     while (true) {
-        if (fill_random(keypair->private_key, sizeof(keypair->private_key)) != OK) return ERROR;
-        if (secp256k1_ec_seckey_verify(this->ctx, keypair->private_key) == 1) break;
+        if (fill_random(privkey, sizeof(privkey)) != OK) return ERROR;
+        if (secp256k1_ec_seckey_verify(this->ctx, privkey) == 1) break;
     }
 
-    if (secp256k1_ec_pubkey_create(this->ctx, &pubkey, this->privkey) != 1) return ERROR;
+    if (secp256k1_ec_pubkey_create(this->ctx, &pubkey, privkey) != 1) return ERROR;
 
     pubkey_len = sizeof(keypair->public_key);
     if (secp256k1_ec_pubkey_serialize(this->ctx, keypair->public_key, &pubkey_len, &pubkey, SECP256K1_EC_COMPRESSED) != 1) return ERROR;
     if (pubkey_len != sizeof(keypair->public_key)) return ERROR;
+    memcpy(keypair->private_key, privkey, sizeof(keypair->private_key));
     return OK;
 }
 
@@ -121,10 +120,10 @@ ErrorCode crypto::CEcdsa::GenerateKeyPair(ecdsa_keypair *keypair) {
  * @param signature
  * @return ErrorCode - OK if successful, ERROR otherwise
  */
-ErrorCode crypto::CEcdsa::Sign(byte *private_key, byte *hash, byte *signature) {
+ErrorCode crypto::CEcdsa::Sign(ecdsa_keypair_t *keypair, byte *hash, byte *signature_out) {
     secp256k1_ecdsa_signature sig;
-    if (secp256k1_ecdsa_sign(this->ctx, &sig, hash, private_key, secp256k1_nonce_function_rfc6979, NULL) != 1) return ERROR;
-    if (secp256k1_ecdsa_signature_serialize_compact(this->ctx, signature, &sig) != 1) return ERROR;
+    if (secp256k1_ecdsa_sign(this->ctx, &sig, hash, keypair->private_key, secp256k1_nonce_function_rfc6979, nullptr) != 1) return ERROR;
+    if (secp256k1_ecdsa_signature_serialize_compact(this->ctx, signature_out, &sig) != 1) return ERROR;
     return OK;
 }
 
@@ -171,7 +170,7 @@ ErrorCode crypto::CEcdsa::PublicKeyFromPrivate(byte *private_key, byte *public_k
  * @brief Shows the keypair in a human-readable format in the terminal.
  * @param keypair
  */
-void crypto::CEcdsa::PrintKeyPair(ecdsa_keypair *keypair) {
+void crypto::CEcdsa::PrintKeyPair(ecdsa_keypair_t *keypair) {
     printf("Private key: ");
     print_hex(keypair->private_key, sizeof(keypair->private_key));
     printf("Public key: ");
