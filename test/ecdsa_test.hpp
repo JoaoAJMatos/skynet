3ccb9db70f7adf4fded68fef43ed9bc8244e08d4
JoaoAJMatos <joaoafonsoj2004@gmail.com>
2023-09-29 11:21:24 +0100

    tests: comments files

--- test/ecdsa_test.hpp
+++ test/ecdsa_test.hpp
@@ -1,3 +1,17 @@
+/**
+ * @file   ecdsa_test.hpp
+ * @author JoaoAJMatos
+ *
+ * @brief ECDSA interface unit tests
+ *	    
+ *
+ * @version 0.1
+ * @date 2023-02-01
+ * @license MIT
+ * @copyright Copyright (c) 2023
+ */
+
+
 /* Skynet includes */
 #include <crypto/ecdsa.hpp>
 #include <crypto/sha256.hpp>
@@ -6,6 +20,9 @@
 
 /* Local includes */
 #include "unipp.hpp"
+
+
+
 
 void EcdsaTest() {
       byte data[] = "Hello World!";
@@ -27,3 +44,25 @@ void EcdsaTest() {
       err = ecdsa.Verify(hash, signature, public_key);
       ASSERT_EQUAL(err, crypto::ECError::OK, "Failed to verify signature (" + crypto::ECErrorToString(err) + ")");
 }
+
+// MIT License
+// 
+// Copyright (c) 2023 João Matos
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
