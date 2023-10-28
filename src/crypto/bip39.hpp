dc03875538e45c26217441c8297dd10aac36da6a
JoaoAJMatos <joaoafonsoj2004@gmail.com>
2023-10-28 15:44:25 +0100

    crypto: refactors bip39 module

--- src/crypto/bip39.hpp
+++ src/crypto/bip39.hpp
@@ -13,96 +13,14 @@
 #define SKYNET_CRYPTO_BIP39_HPP
 
 /** C++ Includes */
-#include <iostream>
 #include <vector>
-#include <cstring>
-#include <random>
-#include <cstdint>
-#include <iomanip>
-#include <sstream>
-#include <fstream>
+#include <string>
 
 /** Skynet Includes */
-#include <crypto/sha256.hpp>
 #include <types.hpp>
 
 namespace crypto::bip39
 {
-      /**
-       * @brief Generates random entropy for the mnemonic generation. (128 bits)
-       *
-       * @return std::vector<byte> The entropy generated.
-       */
-      static std::vector<byte> generate_entropy() {
-            std::vector<byte> entropy(16);
-            std::random_device rd;
-            std::mt19937 gen(rd());
-            std::uniform_int_distribution<> dis(0, 255);
-            for (int i = 0; i < 16; i++) {
-                  entropy[i] = dis(gen);
-            }
-            return entropy;
-      }
-
-      /**
-       * @brief Validates the entropy provided.
-       *
-       * @param entropy The entropy to be validated.
-       * @return true If the entropy is valid.
-       */
-      static bool validate_entropy(std::vector<byte> entropy) {
-            return (entropy.size() % 4 != 0 || entropy.size() < 16 || entropy.size() > 32);
-      }
-
-      /**
-       * @brief Calculates the checksum for the entropy provided.
-       *
-       * @param entropy The entropy to be used.
-       * @return byte The checksum calculated.
-       */
-      static byte calculate_checksum(std::vector<byte> entropy) {
-            std::vector<byte> hash;
-            crypto::hashing::SHA256(entropy.data(), entropy.size(), hash.data());
-            return hash[0] >> (8 - entropy.size() / 4);
-      }
-
-      /**
-       * @brief Loads the word list from the file provided.
-       * 
-       * @param filename 
-       * @return std::vector<std::string> 
-       */
-      static std::vector<std::string> load_word_list(std::string filename) {
-            std::vector<std::string> word_list;
-            std::ifstream file(filename);
-            if (!file.is_open()) {
-                  throw std::runtime_error("Could not open file.");
-            }
-            std::string line;
-            while (std::getline(file, line)) {
-                  word_list.push_back(line);
-            }
-            file.close();
-            return word_list;
-      }
-
-      /**
-       * @brief Splits the given mnemonic into words.
-       * 
-       * @param mnemonic 
-       * @return std::vector<std::string> 
-       */
-      static std::vector<std::string> split_mnemonic_into_words(std::string mnemonic) {
-            std::vector<std::string> words;
-            std::stringstream ss(mnemonic);
-            do {
-                  std::string word;
-                  ss >> word;
-                  words.push_back(word);
-            } while (ss);
-            return words;
-      }
-
       /**
        * @brief Generates a mnemonic from the entropy provided.
        *
@@ -110,28 +28,7 @@ namespace crypto::bip39
        * @param word_list_file The file containing the word list.
        * @return std::string The mnemonic generated.
        */
-      std::string generate_mnemonic(std::vector<byte> entropy, std::string word_list_file) {
-            if (!validate_entropy(entropy)) {
-                  throw std::runtime_error("Invalid entropy provided.");
-            }
-            
-            std::vector<std::string> word_list = load_word_list(word_list_file);
-
-            std::string mnemonic;
-            for (int i = 0; i < entropy.size() * 8 / 32; i++) {
-                  mnemonic += word_list[(entropy[i * 4] << 24 | 
-                                         entropy[i * 4 + 1] << 16 | 
-                                         entropy[i * 4 + 2] << 8 | 
-                                         entropy[i * 4 + 3]) % 
-                                         word_list.size()];
-
-                  if (i != entropy.size() * 8 / 32 - 1) {
-                        mnemonic += " ";
-                  }
-            }
-
-            return mnemonic;
-      }
+      std::string generate_mnemonic(std::vector<byte> entropy, std::string word_list_file);
 
       /**
        * @brief Decodes de mnemonic provided and outputs the seed.
@@ -139,49 +36,7 @@ namespace crypto::bip39
        * @param[i] mnemonic 
        * @param[o] seed 
        */
-      void decode_mnemonic(std::string mnemonic, std::string word_list_file, byte* seed) {
-            std::vector<std::string> words = split_mnemonic_into_words(mnemonic);
-            std::vector<std::string> word_list = load_word_list(word_list_file);
-            std::vector<byte> data(words.size() * 4);
-
-            for (const std::string& word : words) {
-                  /** Find the word's index in the list */
-                  int index = -1;
-                  for (int i = 0; i < word_list.size(); i++) {
-                        if (word_list[i] == word) {
-                              index = i;
-                              break;
-                        }
-                  }
-
-                  if (index == -1) throw std::runtime_error("Word not found in the word list. Word: " + word);
-
-                  /** Add the word's index as a 2 byte value to the data */
-                  data.push_back(static_cast<byte>(index >> 8));
-                  data.push_back(static_cast<byte>(index & 0xFF));
-            }
-
-            if (data.size() % 4 != 0 || data.size() < 16 || data.size() > 32) {
-                  throw std::runtime_error("Invalid data length. Expected 16, 20, 24, 28 or 32 bytes; got " + std::to_string(data.size()));
-            }
-
-            /** Extract the checksum from the last byte and verify it */
-            byte checksum = data.back() & 0xF;
-            for (size_t i = 0; i < data.size() - 1; ++i) {
-                  checksum ^= data[i];
-            }
-
-            if (checksum != 0) {
-                  throw std::runtime_error("Invalid checksum.");
-            }
-
-            /** Remove the checksum from the data */
-            data.pop_back();
-
-            /** Copy the data to the seed and clear the data */
-            memcpy(seed, data.data(), data.size());
-            memset(data.data(), 0, data.size());
-      }
+      void decode_mnemonic(std::string mnemonic, std::string word_list_file, byte* seed);
 } // namespace crypto::bip39
 
 #endif // SKYNET_CRYPTO_BIP39_HPP
