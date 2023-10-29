7533243cba85d1a3b9d8f3a7dd945a20281f24b5
JoaoAJMatos <joaoafonsoj2004@gmail.com>
2023-10-29 22:24:21 +0000

    start transaction structure shift
    I have to push this even tho its not finished bc I have to go to uni. Basically I will be refactoring the transaction structure so it reflects the structure of an actual Bitcoin transaction better

--- src/transaction.hpp
+++ src/transaction.hpp
@@ -15,6 +15,9 @@
 #ifndef SKYNET_TRANSACTION_HPP
 #define SKYNET_TRANSACTION_HPP
 
+/** C++ Includes */
+#include <memory>
+
 /** Skynet includes */
 #include <types.hpp>
 #include <crypto/ecdsa.hpp>
@@ -22,41 +25,40 @@
 
 namespace skynet
 {
-      /**
-       * @brief The transaction OutputMap struct stores all
-       *        the information about a transaction regarding
-       *        the sender and the recipient.
-       */
-      struct OutputMap
-      {
-            crypto::ecdsa::PublicKey recipient;                  /** The recipients wallet address */
-            int amount;                                          /** The amount of coins to be sent */
-            crypto::ecdsa::PublicKey sender;                     /** The sender's wallet address */
-            int balance;                                         /** The sender's balance after the transaction */
+      /** Type Alaises */
+      using TransactionHash = byte[crypto::hashing::SHA256_HASH_SIZE];
 
-            /** Serializing/Deserializing */
-            void ToBytes(byte *bytes);
-            void FromBytes(byte *bytes);
+      /**
+       * @brief The TransactionInput struct represents a single input in a transaction.
+       *        It contains the hash of the previous transaction's output and the index
+       *        
+       */
+      struct TransactionInput
+      {
+            byte prevTransactionOutput[crypto::hashing::SHA256_HASH_SIZE]; /** The hash of the previous transaction's output (TXID) */
+            int prevTransactionOutputIndex;                                /** The index of the previous transaction's output (VOUT) */
+            crypto::ecdsa::PublicKey sender;                               /** The sender's wallet address */
+            crypto::ecdsa::Signature signature;                            /** The signature of the sender */
+
+            TransactionInput(byte prevTransactionOutput[crypto::hashing::SHA256_HASH_SIZE], int prevTransactionOutputIndex, crypto::ecdsa::PublicKey sender, crypto::ecdsa::Signature signature)
+            {
+                  memcpy(this->prevTransactionOutput, prevTransactionOutput, crypto::hashing::SHA256_HASH_SIZE);
+                  this->prevTransactionOutputIndex = prevTransactionOutputIndex;
+                  memcpy(this->sender, sender, crypto::ecdsa::COMPRESSED_PUBLIC_KEY_SIZE);
+                  memcpy(this->signature, signature, crypto::ecdsa::SERIALIZED_SIGNATURE_SIZE);
+            }
       };
 
-      /**
-       * @brief The input map contains critical information for the validation of the transaction
-       *        like the timestamp of when the transaction was emmited, the balance the sender had before
-       *        making the transaction, the public address of the sender and most importantly the transaction's
-       *        digital signature.
-       * 
-       *        The transaction signature is obtained by signing a hash of the output map with the sender's private key
-       */
-      struct InputMap
+      struct TransactionOutput
       {
-            time_t timestamp;                                    /** The timestamp of the transaction */
-            int balance;                                         /** The sender's balance before the transaction */
-            crypto::ecdsa::PublicKey sender;                     /** The sender's wallet address */
-            crypto::ecdsa::Signature signature;                  /** The transaction's digital signature */
+            int value;                                          /** The amount of coins to be sent */
+            crypto::ecdsa::PublicKey recipient;                 /** The recipients wallet address */
 
-            /** Serializing/Deserializing */
-            void ToBytes(byte *bytes);
-            void FromBytes(byte *bytes);
+            TransactionOutput(int value, crypto::ecdsa::PublicKey recipient)
+            {
+                  this->value = value;
+                  this->recipient = recipient;
+            }
       };
 
       /**
@@ -85,6 +87,13 @@ namespace skynet
              */
             std::string ToString() const;
 
+            /**
+             * @brief Returns the hash of the transaction
+             * 
+             * @return std::unique_ptr<byte[]> 
+             */
+            std::unique_ptr<byte[]> Hash() const;
+
             /** Getters */
             [[nodiscard]] InputMap GetInputMap() const { return input_map; }
             [[nodiscard]] OutputMap GetOutputMap() const { return output_map; }
@@ -99,17 +108,16 @@ namespace skynet
              * @return int 
              */
             int GetFeeEarnings();
+
       private:
+            time_t timestamp;                                    /** The timestamp of the transaction */
             InputMap input_map;
             OutputMap output_map;
 
-            byte id[crypto::hashing::SHA256_HASH_SIZE];
+            TransactionID id;
             time_t locktime;
             float version;
             float fee;
-
-            /** Get transaction hash */
-            void Hash(byte *hash);
       };
 }
 
